3aec2ae82a03006e7ef473e4c2fea09ac0f26257
Thomas Wallsmith <thomaswallsmith@gmail.com>
2023-02-13 22:10:27 -0500

    Added fade out animations and tweaks
    - Changed .gitignore to include exclusions for Node.js. Node.js isn't being used but I was exploring it
    - Changed education.html to work with JavaScript changes to allow for fade out animations. Currently only implemented for hte 'View Collegiate Academic History' button.
    - Changed education.js to be organized a little better, and support fade out animations. Looked into using jQuery to load new pages but didn't get it working. Another day!
    - Very minor changes to style.css aswell, mainly a byproduct of some failed tweaks I was implementing.

--- www/js/education.js
+++ www/js/education.js
@@ -1,30 +1,65 @@
 // Thomas Wallsmith
 // Fades in the children of a html class with the id "fadeInContent"
+const FADE_TIMER_OFFSET_MS = 750;
 
-window.onload = fadeInSections;
-function fadeInSections()
+window.onload = fadeInContent;
+
+// Content Swapping //
+function swapContentToCollegiate() // TODO
+{
+     // Fade out previous content
+     fadeOutContent();
+}
+// Animation //
+function fadeOutContent()
+{
+     // Manual fading of subheading to be replaced with title of new content
+     document.getElementById('subheading').classList.add("fade-out");
+
+     // Fading each content section
+     let sections = extractContentChildren();
+     sections.forEach((section) => fadeOut(section));
+}
+function fadeOut(section)
+{
+     // Fades out a HTML section
+     section.classList.remove("fade-in-1");
+     section.classList.add("fade-out");
+}
+function fadeInContent()
+{
+     // Get array of sections to fade in
+     let sections = extractContentChildren();
+
+     // Iterating over the sections to fade them in
+     let offset = 0;
+     sections.forEach((section) => fadeInWithOffset(section, offset++));
+
+     // Removing the fade-in-1 class from the sections
+     sections.forEach((section) => section.classList.remove('fade-in-1'));
+
+     // Report to the console we are done
+     console.log("fadeInContent() is done!");
+}
+function fadeInWithOffset(section, offset)
+{
+     // Does some wacky JavaScript stuff to delay the fade in effect
+     sleep(FADE_TIMER_OFFSET_MS * offset)
+          .then(() => section.removeAttribute("hidden"))
+          .then(() => section.classList.add('fade-in-1'));
+}
+// HTML/JS Utility //
+function extractContentChildren()
 {
      // Populating sections to do the fade in on
-     let fadeInContent = document.getElementById("fadeInContent").children;
+     let fadeInContent = document.getElementById("content").children;
      let sections = [];
      for (let i = 0; i < fadeInContent.length; i++)
      {
           sections.push(fadeInContent.item(i));
      }
-
-     // Iterating over the sections to fade them in
-     let offset = 0;
-     sections.forEach((section) => fadeInWithOffset(section, offset++));
+     return sections;
 }
-
-function fadeInWithOffset(section, offset)
-{
-     // Does some wacky JavaScript stuff to delay the fade in effect
-     sleep(1000*offset)
-          .then(() => section.removeAttribute("hidden"))
-          .then(() => section.classList.add('fade-in-1'));
-}
-
 function sleep(ms)
 {
      return new Promise(resolve => setTimeout(resolve, ms));
